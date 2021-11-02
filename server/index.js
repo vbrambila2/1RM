9b5c0a9ef7b0502225a087d1eb877c57ed10621c
Victor Brambila <victorbrambila22@gmail.com>
2021-11-02 14:57:24 -0700

    changes to server/index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,21 @@ app.use('/movements', movementRoutes);
 
 app.get('/', (req, res) => {
     res.send('Your one rep maxes, together');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
 });
+
+process.once('SIGUSR2', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+  
+  process.on('SIGINT', function () {
+    // this is only called on ctrl+c, not restart
+    process.kill(process.pid, 'SIGINT');
+  });
 
 const PORT = process.env.PORT || 5000;
 
